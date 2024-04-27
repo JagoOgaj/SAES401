717779cc670dff101d4b7631efefe19776b2bf85
Samy93000 <salu27oki@gmail.com>
2024-04-27 14:33:38 +0200

    Update

--- app/src/main/java/com/example/saes401/entities/Player.java
+++ app/src/main/java/com/example/saes401/entities/Player.java
@@ -1,17 +1,18 @@
 package com.example.saes401.entities;
 
+import com.example.saes401.helper.GameConstant;
 import com.example.saes401.utilities.Inventory;
 
-public class Player {
-    Characters character;
-    private int HP;
-    private int DAMAGE;
+public class Player extends GameCharacter{
     private Inventory inventory;
+    private int coin;
+    public Player(String name, int HP, int damage, int coin){
+        super(name, HP, damage);
+        inventory = new Inventory(GameConstant.DEFAULT_INVENTORY_SLOT);
+        this.coin = coin;
 
-    public Player(Characters character){
-        HP = character.getHp();
-        DAMAGE = character.getDamage();
-        inventory = new Inventory(character.getInventorySlot());
     }
+
+
 
 }
--- app/src/main/java/com/example/saes401/utilities/Inventory.java
+++ app/src/main/java/com/example/saes401/utilities/Inventory.java
@@ -3,23 +3,28 @@ package com.example.saes401.utilities;
 
 public class Inventory {
     private Item[] items;
+    private int slots;
 
     public Inventory(int slots){
         items = new Item[slots];
+        this.slots = slots;
     }
 
-    private Item setItemsInventory(int i, Item newItem){
+    public Item setItemsInventory(int i, Item newItem){
         Item item = items[i];
         items[i] = newItem;
         return item;
     }
 
-
-
-    private Item[] getItemsInventory(){
-        return items;
+    public void setCapacityInventory(int newCapa){
+        Item[] newItems = new Item[newCapa];
+        for(int i = 0; i < getCurentLength(); i++)
+            newItems[i] = items[i];
+        items = newItems;
     }
-    private boolean isEmptyInventory(){
-        return items.length == 0;
-    }
+
+    public int getCurentLength(){return items.length;}
+    public Item[] getItemsInventory(){return items;}
+    public boolean isEmptyInventory(){return items.length == 0;}
+    public boolean isFullInventory(){return items.length == slots;}
 }
